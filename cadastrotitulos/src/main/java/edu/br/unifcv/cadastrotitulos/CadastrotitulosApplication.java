7bc32e2e9956c05ca6b7baf876b87d8b3db7a496
Fabio <fabio_dev@outlook.com>
2020-10-30 19:14:26 -0300

    primeiro commit

---
+++ cadastrotitulos/src/main/java/edu/br/unifcv/cadastrotitulos/CadastrotitulosApplication.java
@@ -0,0 +1,13 @@
+package edu.br.unifcv.cadastrotitulos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrotitulosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrotitulosApplication.class, args);
+	}
+
+}
---
+++ cadastrotitulos/src/test/java/edu/br/unifcv/cadastrotitulos/CadastrotitulosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.br.unifcv.cadastrotitulos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrotitulosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
